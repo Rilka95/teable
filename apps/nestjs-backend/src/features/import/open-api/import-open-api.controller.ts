67b4b9954b109036780a3cb160744e82a5e5a82c
xing_cx <caoxing9@gmail.com>
2024-03-21 17:11:57 +0800

    feat: support increment import

--- apps/nestjs-backend/src/features/import/open-api/import-open-api.controller.ts
+++ apps/nestjs-backend/src/features/import/open-api/import-open-api.controller.ts
@@ -1,7 +1,14 @@
 /* eslint-disable sonarjs/no-duplicate-string */
-import { Controller, Get, UseGuards, Query, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, UseGuards, Query, Post, Body, Param, Patch } from '@nestjs/common';
 import type { IAnalyzeVo, ITableFullVo } from '@teable/core';
-import { analyzeRoSchema, IAnalyzeRo, IImportOptionRo, importOptionRoSchema } from '@teable/core';
+import {
+  analyzeRoSchema,
+  IAnalyzeRo,
+  IImportOptionRo,
+  importOptionRoSchema,
+  IInplaceImportOptionRo,
+  inplaceImportOptionRoSchema,
+} from '@teable/core';
 import { ZodValidationPipe } from '../../../zod.validation.pipe';
 import { PermissionGuard } from '../../auth/guard/permission.guard';
 import { ImportOpenApiService } from './import-open-api.service';
@@ -23,5 +30,14 @@ export class ImportController {
     @Body(new ZodValidationPipe(importOptionRoSchema)) importRo: IImportOptionRo
   ): Promise<ITableFullVo[]> {
     return await this.importOpenService.createTableFromImport(baseId, importRo);
+  }
+
+  @Patch(':tableId')
+  async inplaceImportTable(
+    @Param('tableId') tableId: string,
+    @Body(new ZodValidationPipe(inplaceImportOptionRoSchema))
+    inplaceImportRo: IInplaceImportOptionRo
+  ): Promise<void> {
+    return await this.importOpenService.inplaceImportTable(tableId, inplaceImportRo);
   }
 }
